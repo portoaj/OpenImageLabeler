94894d2487d842d4f1f27a3c8f94919b5d6dcd3c
portoaj <andrewmasek@gmail.com>
2020-07-05 02:24:16 -0700

    Setup local variables and object templates

--- js/annotator.js
+++ js/annotator.js
@@ -1,3 +1,27 @@
+let mode = null;
+let images = null;
+/*Image Object structure
+{
+    'base64': base64,
+    'name': name,
+    'relpath': relpath,
+    'height': -1,
+    'width': -1,
+    'annotations': [],
+    'annotated': false
+}
+*/
+/*Annotation Object structure
+{
+    'label': label,
+    'points': [...],
+    'mode': mode
+}
+*/
+
+let selectedAnnotation = null;
+let selectedLabel = null;
+
 function setup() {
     //Create a p5.js canvas in place of the placeholder object
     let placeholder = select('#placeholder');
@@ -6,5 +30,7 @@ function setup() {
     fill(240, 98, 146);
     noStroke();
     rect(0, 0, width, height);
-    $(window).resize(() => {resizeCanvas(placeholder.size()['width'], placeholder.size()['height'])});
+    $(window).resize(() => {
+        resizeCanvas(placeholder.size()['width'], placeholder.size()['height'])
+    });
 }
